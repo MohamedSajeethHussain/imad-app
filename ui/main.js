05157d9f1dd9aa73ccc7fb03faee7f6fe90689e5
MohamedSajeethHussain <30789216+MohamedSajeethHussain@users.noreply.github.com>
2017-08-14 00:10:06 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ console.log('Loaded!');
 //counter code
 
 var button = document.getElementById('counter');
-button.onClick = function(){
+button.onclick = function(){
     //create a request to the counter 
     var request = new XMLHttpRequest();
     
@@ -30,4 +30,23 @@ button.onClick = function(){
     request.send(null);
 
    
+};
+
+//submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  
+  var names = ['name1', 'name2', 'name3', 'name4'];  
+var list = '';
+for(var i=0; i<names.length;i++){
+    list += '<li>' + names[i] + '</li>';
+
+}
+
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+    
 };
