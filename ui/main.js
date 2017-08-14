613ce2b8cb12ba6e6f44098cdad5462fd309751d
MohamedSajeethHussain <30789216+MohamedSajeethHussain@users.noreply.github.com>
2017-08-13 23:33:39 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,18 @@ console.log('Loaded!');
 
 //change the text for testing purpose
 
-var element = document.getElementById("text-content");
-element.innerHTML = "html content changed from main.js";
+//var element = document.getElementById("text-content");
+//element.innerHTML = "html content changed from main.js";
+
+//counter code
+
+var button = document.getElementById("counter");
+button.onClick = function(){
+    //make a request to the counter endpoint
+    // capture the response and store it in a variable
+    //render the variable in the correct span
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
