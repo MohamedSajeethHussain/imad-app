e13a3c46e284eba89dd71709a9a8d6cf0d6672cb
MohamedSajeethHussain <30789216+MohamedSajeethHussain@users.noreply.github.com>
2017-08-16 22:51:09 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'sajeethhussain',
+    database: 'sajeethhussain',
+    host: 'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -96,6 +105,22 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+//DB test case
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+    // make a select request
+    
+    // return a response with the results
+    pool.query('SELECT * FROM test', fucntion (err, result) {
+      if(err){
+          res.status(500).send(err.toString());
+      }  else {
+          res.send(JSON.stringify(result.rows));
+      }
+    
+    });
 });
 //counter
 var counter =0;
